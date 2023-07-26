afd692841e80515f1cae9ec2738358ec2be4ddfd
Felix Cantet <felix.cantet@gmail.com>
2023-07-26 16:35:47 +0200

    Add basic API and foldouts UI Elements. Add Unit Test for all of them

---
+++ Runtime/VisualElements/FoldoutToggle.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace FCA.VisualElements
+{
+    public class FoldoutToggle : FoldoutTitle
+    {
+        private Toggle m_toggle;
+        
+        public Toggle toggle => m_toggle;
+        public FoldoutToggle(string title, bool defaultState) : base(title)
+        {
+            this.m_toggle = this.AddChild(new Toggle()
+            {
+                style =
+                {
+                    position = Position.Absolute,
+                    left = 10,
+                    height = 20,
+                    width = 20
+                },
+                value = defaultState
+            });
+            _value = defaultState;
+        }
+
+        public void RegisterToggleCallback(EventCallback<ChangeEvent<bool>> evt)
+        {
+            m_toggle.RegisterValueChangedCallback(evt);
+        }
+    }
+}
